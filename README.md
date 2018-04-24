70ea9f0b3ec16a01cc246a3d63baf5ac4b1b1c6d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-24 21:58:08 +0000

    Deploy ProLoser/MailChimp-to-SendInBlue to github.com/ProLoser/MailChimp-to-SendInBlue.git:gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# MailChimp-to-SendInBlue
-This converts MailChimp tags and HTML Templates to a format that can be pasted into SendInBlue without data loss
---
+++ bundle.js
@@ -0,0 +1,40 @@
+(function () {
+  'use strict';
+
+  var output = document.getElementById('output');
+  var upload = document.getElementById('upload');
+  var tokens = document.getElementById('tokens');
+  var result = '';
+  var TOKENS_PATTERN = /\*\|\w+\|\*/gi;
+  var suggestions = {
+    '*|EMAIL|*': '{EMAIL}'
+  };
+
+  upload.addEventListener('change', function () {
+
+    // generate a new FileReader object
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+      // Reset the file input
+      upload.value = null;
+      // Copy file contents to textarea
+      output.value = result = reader.result;
+
+      var matches = result.match(TOKENS_PATTERN);
+      matches = matches.map(function (match) {
+        if (suggestions[match]) {
+          output.value = output.value.replace(match, suggestions[match]);
+          return '<li>' + match + ' => ' + suggestions[match];
+        } else {
+          return '<li>' + match;
+        }
+      });
+      tokens.innerHTML = matches.join('');
+    };
+
+    // when the file is read it triggers the onloadend event above
+    reader.readAsText(upload.files[0]);
+  });
+
+}());
